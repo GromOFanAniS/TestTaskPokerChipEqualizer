2984837db37d3c1100c95042b7a6628d19d10f95
GromOFanAniS <grisha.petin@gmail.com>
2025-02-02 04:08:32 +0600

    Implement user input in console app

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ConsoleApp
+﻿using PokerChipEqualizerLib;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> chips = new();
+
+            string input = Console.ReadLine();
+
+            foreach (var chip in input.Split(' '))
+            {
+                chips.Add(int.Parse(chip));
+            }
+
+            Console.WriteLine(Equalizer.EqualizeSteps(chips));
         }
     }
 }
