98e3c1ae6b4f77ce539a47a1a5710122dc076ff0
GromOFanAniS <grisha.petin@gmail.com>
2025-02-02 01:54:52 +0600

    Create tests for Equalizer

--- PokerChipEqualizerLib.Tests/UnitTest1.cs
+++ PokerChipEqualizerLib.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using PokerChipEqualizerLib;
+
 namespace PokerChipEqualizerLib.Tests
 {
     public class UnitTest1
@@ -5,7 +7,22 @@ namespace PokerChipEqualizerLib.Tests
         [Fact]
         public void Test1()
         {
+            int[] chips = { 1, 5, 9, 10, 5 };
+            Assert.Equal(12, Equalizer.EqualizeSteps(chips));
+        }
 
+        [Fact]
+        public void Test2()
+        {
+            int[] chips = { 1, 2, 3 };
+            Assert.Equal(1, Equalizer.EqualizeSteps(chips));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] chips = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 2 };
+            Assert.Equal(1, Equalizer.EqualizeSteps(chips));
         }
     }
 }
