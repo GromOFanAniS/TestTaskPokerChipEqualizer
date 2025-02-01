c67ac4ef286495a24392ba62e0e73f57ad885b0d
GromOFanAniS <grisha.petin@gmail.com>
2025-02-02 02:09:13 +0600

    Create ListExtenstions

---
+++ PokerChipEqualizerLib/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerChipEqualizerLib
+{
+    internal static class ListExtensions
+    {
+        public static int MyIndex(this IList<int> list, int index)
+        {
+            return 0;
+        }
+    }
+}
