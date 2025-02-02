cae7589c3482c729b7131abf83bc2503ff02bbc2
GromOFanAniS <grisha.petin@gmail.com>
2025-02-02 22:45:05 +0600

    Rewrite algorithm for possible fix

--- PokerChipEqualizerLib/Equalizer.cs
+++ PokerChipEqualizerLib/Equalizer.cs
@@ -15,65 +15,98 @@
             return true;
         }
 
+        private static List<(int index, int value)> GetListGreaterThanTarget(IList<int> chipCounts)
+        {
+            int targetChips = chipCounts.Sum() / chipCounts.Count;
+            List<(int, int)> output = new();
+            for (int i = 0; i < chipCounts.Count; i++)
+            {
+                if (chipCounts[i] > targetChips)
+                {
+                    output.Add((i, chipCounts[i]));
+                }
+            }
+            return output;
+        }
+
         public static int EqualizeSteps(IList<int> chipCounts)
         {
             List<int> output = new List<int>(chipCounts);
+            List<(int index, int value)> greaterList = GetListGreaterThanTarget(output);
             int targetChips = output.Sum() / output.Count;
-            int minIndex = 0;
-            int minValue = output[minIndex];
             int stepCount = 0;
-            
-            for (int i = 0; i < output.Count; i++)
-            {
-                if (output[i] < targetChips && output[i] < minValue)
-                {
-                    minIndex = i;
-                    minValue = output[minIndex];
-                }
-            }
 
-            if (minValue >= targetChips)
+            if (greaterList.Count < 1)
             {
                 return stepCount;
             }
 
+            int lookStep = 1;
+            int greaterIndex = 0;
+
             while (!IsEqualized(output))
             {
-                int indexSteps = output.Count / 2;
-                for (int i = 1; i <= indexSteps; i++)
+                foreach (var el in output)
                 {
-                    int left = output.MyIndexValue(minIndex - i);
-                    int right = output.MyIndexValue(minIndex + i);
-                    if (left <= targetChips && right <= targetChips)
-                    {
-                        continue;
-                    }
+                    Console.Write($"{el} ");
+                }
+                Console.WriteLine("");
 
-                    if (left >= right)
-                    {
-                        output[output.MyIndex(minIndex - i)]--;
-                        output[output.MyIndex(minIndex - i + 1)]++;
-                        break;
-                    }
-                    output[output.MyIndex(minIndex + i)]--;
-                    output[output.MyIndex(minIndex + i - 1)]++;
-                    break;
+                greaterList = GetListGreaterThanTarget(output).OrderByDescending(x => x.value).ToList();
+
+                if (greaterIndex >= greaterList.Count)
+                {
+                    greaterIndex = 0;
                 }
 
-                stepCount++;
+                int centerIndex = greaterList[greaterIndex].index;
+                int leftIndex = output.MyIndex(centerIndex - lookStep);
+                int rightIndex = output.MyIndex(centerIndex + lookStep);
+                int centerLeftIndex = output.MyIndex(centerIndex - 1);
+                int centerRightIndex = output.MyIndex(centerIndex + 1);
+                int leftNeed = targetChips - output.MyIndexValue(leftIndex);
+                int rightNeed = targetChips - output.MyIndexValue(rightIndex);
 
-                if (!IsEqualized(output))
+                if (leftNeed <= 0 && rightNeed <= 0)
                 {
-                    minIndex = 0;
-                    minValue = output[minIndex];
-                    for (int i = 0; i < output.Count; i++)
+                    greaterIndex++;
+                    if (greaterIndex >= greaterList.Count)
                     {
-                        if (output[i] < targetChips && output[i] < minValue)
-                        {
-                            minIndex = i;
-                            minValue = output[minIndex];
-                        }
+                        lookStep = (leftIndex == rightIndex || lookStep > output.Count / 2) ? 1 : lookStep + 1;
+                        greaterIndex = 0;
                     }
+                    continue;
+                }
+
+                if ((leftNeed <= rightNeed || rightNeed <= 0) && leftNeed > 0)
+                {
+                    for (int i = 0; i < leftNeed; i++)
+                    {
+                        if (output[centerIndex] <= targetChips)
+                        {
+                            break;
+                        }
+                        output[centerLeftIndex]++;
+                        output[centerIndex]--;
+                        stepCount++;
+                    }
+                    lookStep = 1;
+                    continue;
+                }
+
+                if (rightNeed > 0)
+                {
+                    for (int i = 0; i < rightNeed; i++)
+                    {
+                        if (output[centerIndex] <= targetChips)
+                        {
+                            break;
+                        }
+                        output[centerRightIndex]++;
+                        output[centerIndex]--;
+                        stepCount++;
+                    }
+                    lookStep = 1;
                 }
             }
 
